9d28e2f51bd86709614a3851c3819c8554cf8339
Tamara <nikolovskatamara@gmail.com>
2019-05-24 16:34:22 +0200

    Updated java class, print text changed

---
+++ src/FirstClass_LinuxExample.java
@@ -0,0 +1,9 @@
+
+public class FirstClass_LinuxExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first programming example in Linux and git hub command line experience");
+	}
+
+}
