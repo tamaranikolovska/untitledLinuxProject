b6c947669f80ab1b9db07cc6fd44eed470452759
Tamara <nikolovskatamara@gmail.com>
2019-05-27 15:47:41 +0200

    Made some changes in the main class

--- src/FirstClass_LinuxExample.java
+++ src/FirstClass_LinuxExample.java
@@ -3,6 +3,7 @@ public class FirstClass_LinuxExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Print a message
 		System.out.println("My first programming example in Linux and git hub command line experience");
 	}
 
