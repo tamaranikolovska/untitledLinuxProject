02404464b6716c18c6c262aa597d8cc0a8455496
Tamara <nikolovskatamara@gmail.com>
2019-05-27 15:57:42 +0200

    added new comment to the main class

--- src/FirstClass_LinuxExample.java
+++ src/FirstClass_LinuxExample.java
@@ -3,6 +3,7 @@ public class FirstClass_LinuxExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//Add a different comment here
 		System.out.println("My first programming example in Linux and git hub command line experience");
 	}
 
