670bc8f4bc30247e071ad05d28e5ead94b0ab85d
Tamara <nikolovskatamara@gmail.com>
2019-05-29 16:39:00 +0200

    main class changed in branch V01

--- src/FirstClass_LinuxExample.java
+++ src/FirstClass_LinuxExample.java
@@ -5,6 +5,7 @@ public class FirstClass_LinuxExample {
 		// TODO Auto-generated method stub
 		//Add a different comment here
 		//Print a message
+		//ADDING A THIRD COMMENT HERE
 		System.out.println("My first programming example in Linux and git hub command line experience");
 	}
 
