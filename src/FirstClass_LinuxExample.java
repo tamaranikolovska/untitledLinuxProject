f757e2569ec34cc0d9af1b6635293df59614cdb5
Tamara <nikolovskatamara@gmail.com>
2019-05-29 16:30:46 +0200

    new branch V01 is created

--- src/FirstClass_LinuxExample.java
+++ src/FirstClass_LinuxExample.java
@@ -1,6 +1,12 @@
 
 public class FirstClass_LinuxExample {
-
+	
+	public static void NewSecondFunction(int a, int b)
+	{
+		int c = 0;
+		c = a + b;
+		System.out.println(c);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Add a different comment here
